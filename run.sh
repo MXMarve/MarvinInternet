7911c2933e34e64ec9eca003f30a490420dd9b60
MXMarve <95297108+MXMarve@users.noreply.github.com>
2022-06-21 01:55:33 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-sudo apt install python3.9
-echo Updating...
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-echo Updating Dependencies...
-python3.9 get-pip.py
-sudo rm get-pip.py
-pip3.9 install -r requirements.txt
-echo Starting...
-python3.9 main.py
+pip3 install -r requirements.txt
+python3 main.py
